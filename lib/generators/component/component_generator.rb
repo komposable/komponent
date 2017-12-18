a57ab8d8cfcecd5665e33c059f9b525ffb07a102
Florent Ferry <florent@ouvrages-web.fr>
2017-12-18 22:43:26 +0100

    Namespace component name

--- lib/generators/component/component_generator.rb
+++ lib/generators/component/component_generator.rb
@@ -1,7 +1,6 @@
-class ComponentGenerator < Rails::Generators::Base
+class ComponentGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
 
-  argument :component, required: true, desc: "Component name, e.g: button"
   class_option :locale, type: :boolean, default: false
 
   def create_view_file
@@ -17,7 +16,7 @@ class ComponentGenerator < Rails::Generators::Base
   end
 
   def create_rb_file
-    template "rb.erb", component_path + "#{component_name}_component.rb"
+    template "rb.erb", component_path + "#{module_name.underscore}.rb"
   end
 
   def create_locale_files
@@ -31,22 +30,37 @@ class ComponentGenerator < Rails::Generators::Base
 
   def append_frontend_packs
     append_to_file "frontend/components/index.js" do
-      "import \"components/#{component_name}/#{component_name}\";"
+      "import \"components/#{namespace.join("/")}/#{component_name}\";\n"
     end
   end
 
   protected
 
+  def namespaced?
+    namespace.size > 1
+  end
+
+  def namespace
+    name.split(/[:,::,\/]/).reject(&:blank?)
+  end
+
   def component_path
-    "frontend/components/#{component_name}/"
+    if namespaced?
+      path_parts = ["frontend", "components", *namespace]
+    else
+      path_parts = ["frontend", "components", component_name]
+    end
+
+    Pathname.new(path_parts.join("/"))
   end
 
   def module_name
-    "#{component_name}_component".camelize
+    "#{namespace.join("_")}_component".camelize
   end
   
   def component_name
-    component.underscore
+    return name.underscore unless namespaced?
+    namespace.last.underscore
   end
 
   def template_engine
