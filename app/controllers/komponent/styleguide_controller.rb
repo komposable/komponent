3af3dd0b7d3a935b1a22b531acf7a14bf7c028ab
Hans Lemuet <hans@etaminstudio.com>
2018-06-09 18:42:12 +0200

    Fix rescue when missing template
    The previous implementation (with `render ... rescue`) prevented from debugging the example file.
    Whenever an exception was raised, it displayed the fallback partial.

--- app/controllers/komponent/styleguide_controller.rb
+++ app/controllers/komponent/styleguide_controller.rb
@@ -5,11 +5,18 @@ require "komponent/component"
 module Komponent
   class StyleguideController < ::ApplicationController
     layout 'komponent'
+    rescue_from ActionView::MissingTemplate, with: :missing_template
 
     def index; end
 
     def show
       @component = Komponent::Component.find(params[:id])
+    end
+
+    private
+
+    def missing_template
+      render "komponent/styleguide/missing_template"
     end
   end
 end
