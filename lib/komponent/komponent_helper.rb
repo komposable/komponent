ce3b0fb521300a6ee6b78006c218174f9d8c7f33
Hans Lemuet <hans@etaminstudio.com>
2018-07-14 18:12:44 +0200

    Add component_with_doc (alias cdoc) helper for styleguide

--- lib/komponent/komponent_helper.rb
+++ lib/komponent/komponent_helper.rb
@@ -17,4 +17,29 @@ module KomponentHelper
   def komponent_components
     Komponent::Component.all
   end
+
+  def component_with_doc(component_name, locals = {}, options = {}, &block)
+    captured_block = proc { |args| capture(args, &block) } if block_given?
+    captured_output = capture do
+      Komponent::ComponentRenderer.new(
+        controller,
+      ).render(
+        component_name,
+        locals,
+        options,
+        &captured_block
+      )
+    end
+
+    captured_doc = capture do
+      content_tag :pre, class: "komponent-code" do
+        content_tag :code do
+          "= component \"#{component_name}\", #{locals.to_s.gsub(/(:(\w+)\s?=>\s?)/, "\\2: ")}"
+        end
+      end
+    end
+
+    captured_output + captured_doc
+  end
+  alias :cdoc :component_with_doc
 end
