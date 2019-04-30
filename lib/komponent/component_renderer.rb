539675ef3a3d1ef3e5c2dbc7d93c50f373369a4f
Farhad <farhad9801@gmail.com>
2019-05-01 01:13:36 +0400

    rename instance

--- lib/komponent/component_renderer.rb
+++ lib/komponent/component_renderer.rb
@@ -9,8 +9,8 @@ module Komponent
 
     def initialize(controller, view_flow = nil)
       @context = controller.view_context.dup
-      @view_renderer = @context.view_renderer
-      @lookup_context = @view_renderer.lookup_context = @view_renderer.lookup_context.dup
+      @render = @context.view_renderer
+      @lookup_context = @render.lookup_context = @renderer.lookup_context.dup
       @view_flow = view_flow
     end
 
