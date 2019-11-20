471201f7fc604b07d0352461acd39c6c6ef9b498
Nicolas Brousse <n.brousse@pantographe.studio>
2019-11-20 21:52:11 +0100

    Set locale in Komponent::StyleguideController

--- app/controllers/komponent/styleguide_controller.rb
+++ app/controllers/komponent/styleguide_controller.rb
@@ -6,6 +6,8 @@ module Komponent
     rescue_from ActionView::MissingTemplate, with: :missing_template
 
     before_action :set_components
+    around_action :set_locale
+
     def index; end
 
     def show
@@ -20,6 +22,12 @@ module Komponent
 
     def missing_template
       render 'komponent/styleguide/missing_template', status: :not_found
+    end
+
+    def set_locale
+      I18n.with_locale(params[:locale] || I18n.default_locale) do
+        yield
+      end
     end
   end
 end
